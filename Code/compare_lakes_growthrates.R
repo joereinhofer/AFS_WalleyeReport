ad3e917e9ee482190fe6553c028d0384c3c98774
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-12-07 11:57:24 -0600

    Create file, add title and description

---
+++ Code/compare_lakes_growthrates.R
@@ -0,0 +1,6 @@
+# COMPARE GROWTH RATES ACROSS SOME OF THE LARGER LAKES
+# Plot age vs length for multiple lakes on the same plot
+# Regression lines should help compare growth rates
+# Whole dorsal spines used for this plot
+
+
